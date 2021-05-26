a8981df2aa77b4fc34a627937d0646463449a72a
Alexandre Joris <alex@blockchainpartner.fr>
2021-05-27 00:29:56 +0200

    :sparkles: added route /feed + updated collector route to get monthly top

--- wallapi/api/api.py
+++ wallapi/api/api.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 import requests
+import json
 from database import db
+from sqlalchemy.sql.expression import func
 from db.post import Post, posts_schema
 from .collector import Collector
 
@@ -10,10 +12,10 @@ collector = Collector()
 
 @API.route("/collect/<string:subreddit>", methods=["POST"])
 def collect_medias_from_sub(subreddit):
-    req = requests.get("https://api.reddit.com/r/" + subreddit, 
+    req = requests.get("https://api.reddit.com/r/" + subreddit + "/top?t=day", 
                 headers={"User-Agent": "Chaz Inc. v0.0.1"},
                 params={"limit": "100"}
-        )
+    )
     children = req.json()['data']['children']
     collected = 0
     posts = []
@@ -21,14 +23,15 @@ def collect_medias_from_sub(subreddit):
         try:
             post = collector.collect(child['data'], subreddit)
             if post is not None:
-                posts.append(post)
-                collected += 1
+                exists = Post.query.filter_by(content=post.content).first() is not None
+                if not exists:
+                    posts.append(post)
+                    collected += 1
         except KeyError:
             pass
     db.session.add_all(posts)
     db.session.commit()
     return jsonify({"status": "ok", "collected": collected})
-
 
 @API.route("/feed/<string:subreddit>", methods=['GET'])
 def get_feed(subreddit):
@@ -38,4 +41,15 @@ def get_feed(subreddit):
         query = Post.query.filter_by(subreddit=subreddit).offset(offset).limit(limit)
         return jsonify({"feed": posts_schema.dump(query)})
     except (KeyError):
+        return jsonify({"error": "bad request"}), 400
+
+@API.route("/feed", methods=['GET'])
+def get_random_feed():
+    try:
+        nsfw = request.args.get('nsfw', False, type=json.loads)
+        limit = request.args.get('limit', 25, type=int)
+        offset = request.args.get('offset', 0, type=int)
+        query = Post.query.filter_by(nsfw=nsfw).order_by(func.random()).offset(offset).limit(limit)
+        return jsonify({"feed": posts_schema.dump(query)}), 200
+    except KeyError:
         return jsonify({"error": "bad request"}), 400
